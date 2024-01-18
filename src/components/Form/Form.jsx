c350584710ff8f4935dceb8192ecc5d1a8b0bd74
Anton Krupin <krupin_anton@mail.ru>
2024-01-18 20:57:57 +0300

    add switch case for geolocation errors

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,9 +3,33 @@ import React, { useState } from 'react';
 import './Form.css';
 
 const Form = () => {
+  const [coords, setCoords] = useState();
+  const [error, setError] = useState('');
+  const getPosition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition((position) => {
+        setError('');
+        setCoords(position.coords);
+      },
+      (error) => {
+        switch(error.code) {
+          case 1:
+            setError('Вы запретили получение данных геолокации. Разрешите получение данных геолокации в браузере.');
+            break;
+          case 2:
+            setError('Не удалось получить геолокацию.');
+            break;
+          default: 
+           setError('Не удалось получить геолокацию.');
+        }
+      });
+    } else {
+      setError("Геолокация не поддерживается.");
+    }
+  }
   const formHandler = (e) => {
     e.preventDefault();
-    console.log('test');
+    getPosition();
   }
 
   return (
@@ -20,13 +44,22 @@ const Form = () => {
         />
         <button
           type="submit"
+          disabled={error === '' ? false : true}
         >
           Получить информацию
         </button>
       </form>
-      <div className="formErrors">
-        dsfsdfsdfs
-      </div>
+      {error && (
+        <div className="formErrors">
+          {error}
+        </div>
+      )}
+      {coords && (
+        <div className="coords">
+          <span>Долгота: {coords.latitude}</span>
+          <span>Широта: {coords.longitude}</span>
+        </div>
+      )}
     </div>
   )
 };
