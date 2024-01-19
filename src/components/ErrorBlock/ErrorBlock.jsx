782db8dec06f3557b89a4a231ee65caa5314e835
antonkrupin <krupin_anton@mail.ru>
2024-01-19 10:54:32 +0300

    create ErrorBlock component

---
+++ src/components/ErrorBlock/ErrorBlock.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ErrorBlock.css';
+
+const ErrorBlock = () => {
+	return (
+		<div className="errors">
+			<h1>test</h1>
+		</div>
+	)
+};
+
+export default ErrorBlock;
