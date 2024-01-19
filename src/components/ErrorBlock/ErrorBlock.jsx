bd9af27b748f3152bf33f023fc82ccef14436b88
Anton Krupin <krupin_anton@mail.ru>
2024-01-19 19:21:39 +0300

    add error render

--- src/components/ErrorBlock/ErrorBlock.jsx
+++ src/components/ErrorBlock/ErrorBlock.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { fetchError } from '../../slices/selectors';
 
 import './ErrorBlock.css';
 
 const ErrorBlock = () => {
+	const error = useSelector(fetchError);
 	return (
 		<div className="errors">
-			<h1>test</h1>
+			{error}
 		</div>
 	)
 };
