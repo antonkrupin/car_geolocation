7e804bb201227290b227ac1a16a2ea5adf693769
antonkrupin <krupin_anton@mail.ru>
2024-01-19 10:54:46 +0300

    import ErrorBlock component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/Header/Header';
 import Form from './components/Form/Form';
+import ErrorBlock from './components/ErrorBlock/ErrorBlock';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ const App = () => {
     <>
       <Header />
       <Form />
+			<ErrorBlock />
     </>
   )
 }
