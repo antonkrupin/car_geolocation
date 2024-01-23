7ed09b5679dfe11d2166abb7a8d4ee1bc0227266
antonkrupin <krupin_anton@mail.ru>
2024-01-23 15:45:20 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+} from 'react-router-dom';
+
 import Header from './components/Header/Header';
-import Form from './components/Form/Form';
-import ErrorBlock from './components/ErrorBlock/ErrorBlock';
-import CityMap from './components/CityMap/CityMap';
+import GeolocationRequest from './routes/GeolocationRequest';
+import LoginPage from './routes/LoginPage';
+import AccessConfirmation from './routes/AccessConfirmation';
+import FreightOrders from './routes/FreightOrders';
+import FreightOrderDetails from './routes/FreightOrderDetails';
+import FreightOrederRegistration from './routes/FreightOrderRegistration';
+import EarlyOrderSign from './routes/EarlyOrderSign';
+
+import routes from './routes/routes';
 
 import './App.css';
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Form />
-			<ErrorBlock />
-      <CityMap />
-    </>
+    <BrowserRouter>
+			<Header />
+      <main>
+				<Routes>
+					<Route path={routes.geolocationRequest()} element={<GeolocationRequest />} />
+					<Route path={routes.loginPage()} element={<LoginPage />} />
+					<Route path={routes.accessConfirmation()} element={<AccessConfirmation />} />
+					<Route path={routes.freightOrders()} element={<FreightOrders />} />
+					<Route path={routes.freightOrderDetails()} element={<FreightOrderDetails />} />
+					<Route path={routes.freightOrederRegistration()} element={<FreightOrederRegistration />} />
+					<Route path={routes.earlyOrderSign()} element={<EarlyOrderSign />} />
+				</Routes>
+			</main>
+    </BrowserRouter>
   )
 }
 
