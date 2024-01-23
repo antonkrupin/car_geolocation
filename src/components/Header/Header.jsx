fb79d23adb0a6926cb61aac66e61e3c67cc045fe
antonkrupin <krupin_anton@mail.ru>
2024-01-23 15:44:25 +0300

    change header text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
   return (
     <div className="header">
       <img src="clients_severstal.png" alt="Северсталь логотип" />
+			<h4>Электронная очередь</h4>
     </div>
   )
 };
