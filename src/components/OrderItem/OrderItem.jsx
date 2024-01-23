af85eb5370ff800a686ecb13757191db9cf4e9b1
antonkrupin <krupin_anton@mail.ru>
2024-01-23 16:02:54 +0300

    add button order details

--- src/components/OrderItem/OrderItem.jsx
+++ src/components/OrderItem/OrderItem.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const OrderItem = (props) => {
 	const { order } = props;
+	const navigate = useNavigate();
+	const buttonHandler = () => {
+		navigate(`/details/${order[0]}`)
+	};
 	return (
 		<tr>
 			<td>{order[0]}</td>
 			<td>{order[1]}</td>
 			<td>{order[2]}</td>
-			<td>Детали заказа</td>
+			<td><button onClick={buttonHandler}>Детали заказа</button></td>
 		</tr>
 	)
 };
