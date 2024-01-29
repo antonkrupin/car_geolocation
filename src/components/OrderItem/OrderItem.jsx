c3050cf04932b0a5526b15b8ae1c4f2813bad7c8
antonkrupin <krupin_anton@mail.ru>
2024-01-29 12:41:33 +0300

    add icons import

--- src/components/OrderItem/OrderItem.jsx
+++ src/components/OrderItem/OrderItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 
+import truckIcon from '../../images/icons/truck.png';
+import detailButton from '../../images/icons/detailButton.png';
+
 import './OrderItem.css';
 
 const OrderItem = (props) => {
@@ -11,13 +14,13 @@ const OrderItem = (props) => {
 	};*/
 	return (
 		<div className="orderItem">
-			<img src="images/icons/detailButton.png" alt="Иконка грузовика"/>
+			<img src={truckIcon} alt="Иконка грузовика"/>
 			<div>
-				<h3>{order[0]}</h3>
-				<h4>{order[2]}</h4>
-				<h5>{order[1]}</h5>
+				<h3 className="orderItem_id">{order[0]}</h3>
+				<h4 className="orderItem_city">{order[2]}</h4>
+				<h5 className="orderItem_status">{order[1]}</h5>
 			</div>
-			<img src="images/icons/detailButton.png" alt="Детали заказа"/>
+			<button className="orderItem_button"><img src={detailButton} alt="Детали заказа"/></button>
 		</div>
 	)
 };
