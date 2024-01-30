2c9b157da72e673c75cd5f28a8bc9603234d614e
antonkrupin <krupin_anton@mail.ru>
2024-01-30 08:50:11 +0300

    create FilledButton component

---
+++ src/components/FilledButton/FilledButton.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./FilledButton.css";
+
+const FilledButton = (props) => {
+  const { onClick, buttonText, disabled, isSpinner } = props;
+
+  return (
+    <>
+      {isSpinner && (
+        <button onClick={onClick} className="filledButton" type="submit" disabled={disabled}>
+          <svg className="spinner" viewBox="0 0 50 50">
+            <circle
+              className="path"
+              cx="25"
+              cy="25"
+              r="20"
+              fill="none"
+              strokeWidth="1"
+            ></circle>
+          </svg>
+        </button>
+      )}
+      {!isSpinner && (
+        <button onClick={onClick} className="filledButton" type="submit" disabled={disabled}>
+          {buttonText}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default FilledButton;
