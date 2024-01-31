9daa1b27d8e6ad67fc707c1fbb87c1c1b75fdfca
Anton Krupin <krupin_anton@mail.ru>
2024-01-31 10:36:07 +0300

    update Spinner component

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -12,16 +12,18 @@ const Spinner = (props) => {
 		'littleSpinner': !size,
 	});
 	return (
-		<svg className={spinnerClassName} viewBox="0 0 50 50">
-			<circle
-				className="path"
-				cx="25"
-				cy="25"
-				r="20"
-				fill="none"
-				strokeWidth="1"
-			></circle>
-		</svg>
+		<div className="spinnerBlock">
+			<svg className={spinnerClassName} viewBox="0 0 50 50">
+				<circle
+					className="path"
+					cx="25"
+					cy="25"
+					r="20"
+					fill="none"
+					strokeWidth="1"
+				></circle>
+			</svg>
+		</div>
 	)
 };
 
