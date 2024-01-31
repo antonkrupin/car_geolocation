38bc0fd0190b1c21b000767446c1bac4c5a65d7f
Anton Krupin <krupin_anton@mail.ru>
2024-01-31 18:36:59 +0300

    import ErrorBlock component

--- src/routes/AccessConfirmation/AccessConfirmation.jsx
+++ src/routes/AccessConfirmation/AccessConfirmation.jsx
@@ -7,22 +7,23 @@ import { setCode } from "../../slices/mainReducer";
 import { fetchPhone } from "../../slices/selectors";
 
 import BackwardButton from "../../components/BackwardButton/BackwardButton";
-import Spinner from '../../components/Spinner/Spinner';
+import Spinner from "../../components/Spinner/Spinner";
+import ErrorBlock from "../../components/ErrorBlock/ErrorBlock";
 
 import "./AccessConfirmation.css";
 
-const TEST_COUNTER_VALUE = 20;
-const TEST_PHONE_CODE = '3456';
+import { TEST_COUNTER_VALUE, TEST_PHONE_CODE } from "../../assets/TEST_CONST";
 
 const AccessConfirmation = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-	const phone = useSelector(fetchPhone);
+  const phone = useSelector(fetchPhone);
 
   const [counter, setCounter] = useState(TEST_COUNTER_VALUE);
   const [isCounting, setIsCounting] = useState(false);
   const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const {
     register,
@@ -62,32 +63,43 @@ const AccessConfirmation = () => {
     timer();
     //firstInputRef.current.focus();
   }, [isCounting]);
-	
-  const redirectTest = () => {
+
+  const redirectTest = (code) => {
     setLoading(false);
-    navigate(`/personalDataConfirmation/${phone}`);
+    if (Number(code) === Number(TEST_PHONE_CODE)) {
+      navigate(`/personalDataConfirmation/${phone}`);
+    } else {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 6000);
+    }
   };
 
-	const onSubmit = (data) => {
-		const code = Object.values(watch()).join('');
-		if (Number(code) === Number(TEST_PHONE_CODE)) {
-      setLoading(true);
-			dispatch(setCode(code));
-      setTimeout(redirectTest, 2000);
-			//navigate(`/personalDataConfirmation/${phone}`);
-		}
+  const onSubmit = (data) => {
+    const code = Object.values(watch()).join("");
+    setLoading(true);
+    dispatch(setCode(code));
+    setTimeout(() => redirectTest(code), 1000);
   };
 
   return (
     <>
       {!isLoading && (
         <div className="accessConfirmation">
+          {error && (
+            <ErrorBlock
+              show={error}
+              errorTitle="Неправильный код"
+              errorMessage="Попробуйте еще раз или отправьте код повторно"
+            />
+          )}
           <div className="accessConfirmation__content">
             <BackwardButton />
             <h4>Введите код из SMS-сообщения</h4>
             <form onSubmit={handleSubmit(onSubmit)}>
               <input
-                {...register("firstCodeChar", { required: true, })}
+                {...register("firstCodeChar", { required: true })}
                 maxLength="1"
               />
               <input
@@ -105,9 +117,10 @@ const AccessConfirmation = () => {
               />
             </form>
             <h3>
-              Вам отправлен код подтверждения на номер - {hidePhoneNumbers(phone)}
+              Вам отправлен код подтверждения на номер -{" "}
+              {hidePhoneNumbers(phone)}
             </h3>
-    
+
             <button
               onClick={resendAccessCode}
               disabled={counter !== 0 ? true : false}
@@ -120,7 +133,7 @@ const AccessConfirmation = () => {
       {isLoading && (
         <div className="accessConfirmation">
           <div className="accessConfirmation__content">
-            <h4>Введите код из SMS-сообщения</h4>
+            <h4 className="marginTop">Введите код из SMS-сообщения</h4>
             <Spinner size="big" color="blue" />
           </div>
         </div>
@@ -130,5 +143,3 @@ const AccessConfirmation = () => {
 };
 
 export default AccessConfirmation;
-
-
