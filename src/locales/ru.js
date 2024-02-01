4f8030eb9569b8fe715d832a616756c47cbd764e
antonkrupin <krupin_anton@mail.ru>
2024-02-01 15:56:24 +0300

    init i18next

---
+++ src/locales/ru.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  translation: {
+    errorText: {
+			wrongSmsCode: {
+				title: 'Неправильный код',
+				message: 'Попробуйте еще раз или отправьте код повторно',
+			},
+			ordersNotFound: {
+				title: 'По указанному номеру заказы не найдены',
+			},
+			outOfRegistrationZone: {
+				title: 'Вы находитесь не в зоне возможности регистрации',
+				message: 'Повторите попытку ближе к складу',
+			},
+			outOfRegistrationCancelingZone: {
+				title: 'Вы находитесь не в зоне для отмены регистрации',
+				message: 'Повторите попытку ближе к складу',
+			},
+			haveRegesteredOrder: {
+				title: 'У вас уже есть зарегистрированный заказ',
+				message: 'Завершите текущий заказ и попробуйте ещё раз',
+			},
+			bookedLoadingSlot: {
+				title: 'При регистрации произошла ошибка',
+				message: 'Выбранный слот уже занят',
+			},
+			registrationError: {
+				title: 'При регистрации произошла ошибка',
+				message: 'Попробуйте еще раз',
+			}
+		}
+  },
+};
