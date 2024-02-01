3eee7a58ed9a8d1b42aa657e7ac7640e0604e479
antonkrupin <krupin_anton@mail.ru>
2024-02-01 11:33:51 +0300

    add component for testing map positioning

---
+++ src/components/CItyMap/CityMap.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { YMaps, Map, Placemark, Circle, ZoomControl } from '@pbe/react-yandex-maps';
+
+import { fetchCoords } from '../../slices/selectors';
+
+import { TEST_KPP_COORDS, getDistanceFromLatLonInKm } from '../../assets/TEST_CONST';
+
+
+const CityMap = (props) => {
+	const { carCoords } = props;
+  //const coords = useSelector(fetchCoords);
+	const result = getDistanceFromLatLonInKm(TEST_KPP_COORDS[0], TEST_KPP_COORDS[1], carCoords[0], carCoords[1]);
+	console.log(result);
+  return (
+    <YMaps>
+      <div>
+        <Map
+          defaultState={{ center: TEST_KPP_COORDS, zoom: 12 }}
+          width="100%"
+          height="55vh"
+        >
+          <Placemark
+            //modules={["geoObject.addon.balloon"]}
+            geometry={TEST_KPP_COORDS}
+            /*properties={{
+              balloonContentBody:
+                [i18next.t('header.clubs.drevlyanka.location'),i18next.t('header.clubs.drevlyanka.address')].join(',')
+            }}*/
+            options={{
+              //iconLayout: "default#image",
+              preset: "islands#blueCircleDotIcon"
+              //iconImageSize: [70, 70],
+              //iconImageHref: 'images/map_pointer_icon_1.png',
+            }}
+          />
+					<Placemark
+						modules={["geoObject.addon.balloon"]}
+						geometry={carCoords}
+						/*properties={{
+							balloonContentBody:
+								[i18next.t('header.clubs.drevlyanka.location'),i18next.t('header.clubs.drevlyanka.address')].join(',')
+						}}*/
+						options={{
+							//iconLayout: "default#image",
+							preset: "islands#blueAutoIcon"
+							//iconImageSize: [70, 70],
+							//iconImageHref: 'images/map_pointer_icon_1.png',
+						}}
+					/>
+          <Circle 
+            geometry={[TEST_KPP_COORDS, 5000]}
+            options={{
+              draggable: false,
+              fillColor: "#DB709377",
+              strokeColor: "#990066",
+              strokeOpacity: 0.8,
+              strokeWidth: 5,
+            }}
+          />
+          <ZoomControl options={{ float: "right" }} />
+        </Map>
+      </div>
+    </YMaps>
+  )
+};
+
+export default CityMap;
+
+/*
+{coords && (
+	<Placemark
+	modules={["geoObject.addon.balloon"]}
+	geometry={[coords[0], coords[1]]}
+	properties={{
+		balloonContentBody:
+			[i18next.t('header.clubs.drevlyanka.location'),i18next.t('header.clubs.drevlyanka.address')].join(',')
+	}}
+	options={{
+		//iconLayout: "default#image",
+		preset: "islands#blueAutoIcon"
+		//iconImageSize: [70, 70],
+		//iconImageHref: 'images/map_pointer_icon_1.png',
+	}}
+/>
+)}
+*/
