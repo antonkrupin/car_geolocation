16a15aa44dc3dab429892b43855363e8b6ba2761
Anton Krupin <krupin_anton@mail.ru>
2024-02-04 18:58:51 +0300

    add redux usage

--- src/routes/FreightOrderDetails/FreightOrderDetails.jsx
+++ src/routes/FreightOrderDetails/FreightOrderDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { useParams, useNavigate } from "react-router";
+import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 
-import { setModalOpen, setIsLoading } from "../../slices/mainReducer";
-import { fetchOrders, fetchIsModalOpen } from "../../slices/selectors";
+import { setModalOpen, setError } from "../../slices/mainReducer";
+import { fetchOrders, fetchError } from "../../slices/selectors";
 
 import BackwardButton from "../../components/BackwardButton/BackwardButton";
 import FilledButton from "../../components/FilledButton/FilledButton";
@@ -22,11 +22,9 @@ import {
 
 const FreightOrderDetails = () => {
 	const dispatch = useDispatch();
-	const isModalOpen = useSelector(fetchIsModalOpen);
-  const navigate = useNavigate();
-
-  const [error, setError] = useState(false);
+	
 	const [modalType, setModalType] = useState();
+	const error = useSelector(fetchError);
 
   const orders = useSelector(fetchOrders)[1];
   const id = useParams().id;
@@ -43,18 +41,31 @@ const FreightOrderDetails = () => {
   } = orders.filter((order) => order.id === id)[0];
 
   const orderRegistrationHandler = () => {
-    /*const registeredOrders = orders.filter((order) => order.status === 1);
-    if (registeredOrders.length !== 0) {
-      setError('haveRegesteredOrder');
-      setTimeout(() => {
-        setError(false);
-      }, 6000);
-    } else {
-      navigate(`/registration/${id}`);
-    }*/
-		setModalType('registration');
-		//dispatch(setIsLoading());
-		dispatch(setModalOpen());
+    const registeredOrders = orders.filter((order) => order.status === 1);
+		//TEST_CAR_COORDS[0] - не в радиусе
+		//TEST_CAR_COORDS[1] - в радиусе
+		//TEST_CAR_COORDS[2] - не в радиусе, но очень близко
+		//TEST_CAR_COORDS[3] - в радиусе
+		const distance = getDistanceFromLatLonInKm(
+			TEST_KPP_COORDS[0],
+			TEST_KPP_COORDS[1],
+			TEST_CAR_COORDS[1][0],
+			TEST_CAR_COORDS[1][1]
+		);
+		if (registeredOrders.length !== 0 || TEST_REGISTRATION_DISTANCE < distance) {
+			if (registeredOrders.length !== 0) {
+				dispatch(setError('haveRegesteredOrder'));
+				setTimeout(() => {
+					dispatch(setError());
+				}, 6000);
+			} else {
+				dispatch(setError('outOfRegistrationZone'));
+				setTimeout(() => {dispatch(setError())}, 6000);
+			}
+		} else {
+			setModalType('registration');
+			dispatch(setModalOpen());
+		}
   };
 
 	const orderRegistrationCancelHandler = () => {
@@ -69,7 +80,7 @@ const FreightOrderDetails = () => {
 			TEST_CAR_COORDS[1][1]
 		);
 		if (TEST_REGISTRATION_DISTANCE < distance) {
-			setError('outOfRegistrationCancelingZone');
+			dispatch(setError('outOfRegistrationCancelingZone'));
 			setTimeout(() => {setError(false)}, 6000);
 		} else {
 			dispatch(setModalOpen());
@@ -78,72 +89,70 @@ const FreightOrderDetails = () => {
 
   return (
 		<>
-			<ModalWindow id={id} type={modalType}/>
-			{!isModalOpen && (
-				<div className="freightOrderDetails">
-					{error && (
-						<ErrorBlock
-							show={error}
-							error={error}
+			<ModalWindow id={id} modalBody={modalType} />
+			<div className="freightOrderDetails">
+				{error && (
+					<ErrorBlock
+						show={error}
+						error={error}
+					/>
+				)}
+				<div className="freightOrderDetails__content">
+					<BackwardButton />
+					<h1>
+						Заказ <span>{id}</span>
+					</h1>
+					<span className={status && "registered"}>
+						{status ? "Зарегистрирован" : "Не зарегистрирован"}
+					</span>
+					<div className="freightOrderDetails__div">
+						<div className="address">
+							<h5>Адрес</h5>
+							<h6>{address}</h6>
+						</div>
+						<div className="storage">
+							<h5>Склад</h5>
+							<h6>{storage}</h6>
+						</div>
+						<div className="enterDate">
+							<h5>Дата въезда</h5>
+							<h6>{enterDate}</h6>
+						</div>
+						<div className="carNumber">
+							<h5>№ ТС</h5>
+							<h6>{carNumber}</h6>
+						</div>
+						<div className="loadingSlot">
+							<h5>Слот погрузки</h5>
+							<h6>{loadingSlot}</h6>
+						</div>
+						<div className="priority">
+							<h5>Приоритет</h5>
+							<h6>{priority}</h6>
+						</div>
+					</div>
+					<form>
+						<div>
+							<input id="earlyChecOut" type="checkbox" disabled />
+							<label htmlFor="earlyChecOut">Готовность раннего въезда</label>
+						</div>
+						<div>
+							<input id="withOutQueue" type="checkbox" disabled />
+							<label htmlFor="withOutQueue">Без очереди</label>
+						</div>
+					</form>
+					<hr />
+					<OutlinedButton buttonText="Посмотреть схему проезда" />
+					{status ? (
+						<OutlinedButton onClick={orderRegistrationCancelHandler} buttonText="Отменить регистрацию" />
+					) : (
+						<FilledButton
+							onClick={orderRegistrationHandler}
+							buttonText="Зарегистрироваться"
 						/>
 					)}
-					<div className="freightOrderDetails__content">
-						<BackwardButton />
-						<h1>
-							Заказ <span>{id}</span>
-						</h1>
-						<span className={status && "registered"}>
-							{status ? "Зарегистрирован" : "Не зарегистрирован"}
-						</span>
-						<div className="freightOrderDetails__div">
-							<div className="address">
-								<h5>Адрес</h5>
-								<h6>{address}</h6>
-							</div>
-							<div className="storage">
-								<h5>Склад</h5>
-								<h6>{storage}</h6>
-							</div>
-							<div className="enterDate">
-								<h5>Дата въезда</h5>
-								<h6>{enterDate}</h6>
-							</div>
-							<div className="carNumber">
-								<h5>№ ТС</h5>
-								<h6>{carNumber}</h6>
-							</div>
-							<div className="loadingSlot">
-								<h5>Слот погрузки</h5>
-								<h6>{loadingSlot}</h6>
-							</div>
-							<div className="priority">
-								<h5>Приоритет</h5>
-								<h6>{priority}</h6>
-							</div>
-						</div>
-						<form>
-							<div>
-								<input id="earlyChecOut" type="checkbox" disabled />
-								<label htmlFor="earlyChecOut">Готовность раннего въезда</label>
-							</div>
-							<div>
-								<input id="withOutQueue" type="checkbox" disabled />
-								<label htmlFor="withOutQueue">Без очереди</label>
-							</div>
-						</form>
-						<hr />
-						<OutlinedButton buttonText="Посмотреть схему проезда" />
-						{status ? (
-							<OutlinedButton onClick={orderRegistrationCancelHandler} buttonText="Отменить регистрацию" />
-						) : (
-							<FilledButton
-								onClick={orderRegistrationHandler}
-								buttonText="Зарегистрироваться"
-							/>
-						)}
-					</div>
 				</div>
-			)}
+			</div>
 		</>
   );
 };
