dc5608ed2dacf115718f3af1856671e8fb5f2341
Anton Krupin <krupin_anton@mail.ru>
2024-02-06 19:14:41 +0300

    create ModalWindowBody component

---
+++ src/components/ModalWindowBody/ModalWindowBody.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './ModalWindowBody.css';
+
+export const RoadMap = () => {
+  return (
+    <div className="roadMap">
+      <div className="roadMap__content">
+        <h1>Схема проезда</h1>
+      </div>
+    </div>
+  )
+};
+
+export const HomeScreenAdd = () => {
+  return (
+    <div className="homeScreenAdd">
+      <div className="homeScreenAdd__content">
+        <h1>Добавить приложение “Электронная очередь” на рабочий экран</h1>
+        <h4>Вы можете добавить приложение на экран своего смартфона выполнив эти действия:</h4>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
